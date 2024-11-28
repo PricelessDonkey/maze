a5caa256fccc08102bc231060af747cd26c6484e
PricelessDonkey <62314090+PricelessDonkey@users.noreply.github.com>
2024-11-28 20:46:38 +0000

    json changes

--- maze-app/docs/server/angular-app-engine-manifest.mjs
+++ maze-app/docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/maze-app',
+  basePath: '/maze-app/',
   entryPoints: new Map([['', () => import('./main.server.mjs')]]),
 };
   
--- maze-app/docs/server/angular-app-manifest.mjs
+++ maze-app/docs/server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 499, hash: '5a332d4472ad3f47b6ad2f3fe8c41c8e07c355da3f67799a5b4f6890ee75be8f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1012, hash: '93973822e84cfc3f1bde18c5a36060d371262e543fec3316451428ef493f526d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 500, hash: '134c37a0c35a5ce3b3d037e764ce7cdcaa129fcdc90c3fc4992b43ffb3004638', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1013, hash: '64a08f2775eeb85d01111ae854a074a0883744c4da5622d89cba5015a0f5cc8b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-5INURTSO.css', {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
